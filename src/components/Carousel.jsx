c4c218c06eeeec52820b067d1b6a7d041b352e54
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-23 20:51:57 +0530

    add proper styling and width to hero section

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,17 +9,14 @@ import 'swiper/css/pagination';
 export default () => {
   return (
     <Swiper
-      className='w-96 h-96 bg-slate-600'
+      className='w-80 lg:w-1/3 h-48 lg:h-96 bg-green-700 rounded-lg text-white'
       modules={[Pagination, A11y, Autoplay]}
-      spaceBetween={50}
       slidesPerView={1}
       pagination={{ clickable: true }}
-      autoplay={{ delay: 2500, disableOnInteraction: false }}
+      autoplay={{ delay: 2500, disableOnInteraction: true }}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide className='p-8'>Epoch</SwiperSlide>
+      <SwiperSlide className='p-8'>Epoch Workshop</SwiperSlide>
     </Swiper>
   );
 };
