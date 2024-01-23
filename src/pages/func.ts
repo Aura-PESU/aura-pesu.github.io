72ef4880baccb2425da6b70adac5962172acab0c
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-23 19:24:43 +0530

    fix image rounding and alt text and code styling

--- src/pages/func.ts
+++ src/pages/func.ts
@@ -2,18 +2,18 @@ const lines = ["Welcome To AURA", "Best AIML CLUB"];
 let lineIndex = 0;
 let wordIndex = 0;
 let charIndex = 0;
-let intervalId;
+let intervalId: NodeJS.Timer;
 
 function typeText() {
   const currentLine = lines[lineIndex];
   const words = currentLine.split(" ");
 
   if (charIndex < words[wordIndex].length) {
-    document.getElementById(`line${lineIndex + 1}`).textContent +=
+    document.getElementById(`line${lineIndex + 1}`)!.textContent +=
       words[wordIndex][charIndex];
     charIndex++;
   } else {
-    document.getElementById(`line${lineIndex + 1}`).textContent += " "; // Add space between words
+    document.getElementById(`line${lineIndex + 1}`)!.textContent += " "; // Add space between words
     charIndex = 0;
     wordIndex++;
   }
