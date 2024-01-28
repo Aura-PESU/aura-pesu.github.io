eaa5e468b62b7398e8971c7f9120498d5ba6cd33
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-28 14:13:01 +0530

    Update favicon and event images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,22 +1,42 @@
 // import Swiper core and required modules
-import { Pagination, A11y, Autoplay } from 'swiper/modules';
+import { Pagination, A11y, Autoplay } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
+
+import EpochImg from "../assets/epoch.jpg";
+import EpochWorkshopImg from "../assets/workshop.jpeg";
 
 export default () => {
   return (
-    <Swiper
-      className='w-80 lg:w-1/3 h-48 lg:h-96 bg-green-700 rounded-lg text-white'
-      modules={[Pagination, A11y, Autoplay]}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 2500, disableOnInteraction: true }}
-    >
-      <SwiperSlide className='p-8'>Epoch</SwiperSlide>
-      <SwiperSlide className='p-8'>Epoch Workshop</SwiperSlide>
-    </Swiper>
+      <Swiper
+        className="w-80 lg:w-1/3 h-48 lg:h-96 rounded-lg text-white"
+        modules={[Pagination, A11y, Autoplay]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2500, disableOnInteraction: true }}
+      >
+        <SwiperSlide className="">
+          <EventSlide title="Epoch 2023" src={EpochImg.src} />
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <EventSlide title="Epoch Workshop" src={EpochWorkshopImg.src} />
+        </SwiperSlide>
+      </Swiper>
   );
 };
+
+function EventSlide({ title, src }) {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center z-10">{title}</h1>
+      <img
+        src={src}
+        className="w-full h-48 lg:h-96 object-cover text-center z-0"
+        alt={title}
+      />
+    </div>
+  );
+}
